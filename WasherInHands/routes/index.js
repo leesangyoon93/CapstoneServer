799cd46e6a10d69d0b89051ac9235f5d78e301e1
leesangyoon93 <libertyoon@naver.com>
2016-09-19 22:47:08 +0900

    asdf

--- WasherInHands/routes/index.js
+++ WasherInHands/routes/index.js
@@ -62,7 +62,7 @@ module.exports = function (passport) {
                     newUser.userId = req.body.userId;
                     newUser.password = req.body.password;
                     newUser.userName = req.body.userName;
-                    newUser.isAdmin = req.body.isAdmin;
+                    newUser.isAdmin = false;
                     newUser.save(function (err) {
                         if (err) throw err;
                         req.login(newUser, function (err) {
