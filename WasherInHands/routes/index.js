3b146111624bad2845017cbf2ac45121249cc007
leesangyoon93 <libertyoon@naver.com>
2016-11-24 14:20:31 +0900

    asdf

--- WasherInHands/routes/index.js
+++ WasherInHands/routes/index.js
@@ -99,8 +99,8 @@ module.exports = function (passport) {
                         geocoder.geocode(req.body.address, function(err, res) {
                             if(err) return res.json({'result': 'fail'});
                             else {
-                                newWasherRoom.latitude = res.res.results[0].geometry.bounds.location.lat;
-                                newWasherRoom.longtitude = res.res.results[0].geometry.bounds.location.lng;
+                                newWasherRoom.latitude = res.results[0].geometry.bounds.location.lat;
+                                newWasherRoom.longtitude = res.results[0].geometry.bounds.location.lng;
                             }
                         }, {language: 'ko', provider: 'google', api_key: 'AIzaSYAtvE5zlussGrKe2tcMnB9AhqeNmssGQ40', httpAdapter: 'https'});
                         newWasherRoom.members.push(user);
